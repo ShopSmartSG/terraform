d3eefb4c406ed6c58cd9f671646b295185a43ec2
sasmitmati <sasmitmati98@gmail.com>
2024-10-08 09:37:14 +0800

    updated main file to remove support for ssh

--- main.tf
+++ main.tf
@@ -2,27 +2,22 @@ provider "aws" {
   region = "us-east-1" # Change this if you prefer another region
 }
 
-resource "aws_key_pair" "deployer_key" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub") # Ensure you have your public key here
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic from anywhere
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
 }
 
@@ -30,8 +25,9 @@ resource "aws_instance" "test_ec2" {
   ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (Free tier eligible). Update based on your region.
   instance_type = "t2.micro"              # Free tier eligible
 
-  key_name               = aws_key_pair.deployer_key.key_name
-  security_groups        = [aws_security_group.allow_ssh.name]
+  # Remove SSH-related key pair
+  # key_name               = aws_key_pair.deployer_key.key_name
+  security_groups        = [aws_security_group.allow_http.name]
   associate_public_ip_address = true
 
   user_data = <<-EOF
