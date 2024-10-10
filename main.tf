bcc7923c062f20de89bfc2df23a2bc163e262505
sasmitmati <sasmitmati98@gmail.com>
2024-10-11 01:46:42 +0800

    added new VPC and removed test machines

--- main.tf
+++ main.tf
@@ -9,48 +9,48 @@ variable "aws_region" {
 }
 
 
-resource "aws_security_group" "allow_http" {
-  name = "allow_http_temptest"
-  description = "Allow HTTP inbound traffic"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic from anywhere
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
-  }
-}
-
-resource "aws_instance" "test_ec2" {
-  ami           = "ami-0ad522a4a529e7aa8" # Amazon Linux 2 AMI (Free tier eligible). Update based on your region.
-  instance_type = "t2.micro"              # Free tier eligible
-
-  # Remove SSH-related key pair
-  # key_name               = aws_key_pair.deployer_key.key_name
-  security_groups        = [aws_security_group.allow_http.name]
-  associate_public_ip_address = true
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              echo "<h1>Deployed via Terraform</h1>" > /var/www/html/index.html
-              EOF
-
-  tags = {
-    Name = "GitOps-Test-Instance"
-  }
-}
-
-output "instance_public_ip" {
-  value = aws_instance.test_ec2.public_ip
-}
+# resource "aws_security_group" "allow_http" {
+#   name = "allow_http_temptest"
+#   description = "Allow HTTP inbound traffic"
+#
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic from anywhere
+#   }
+#
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+#   }
+# }
+#
+# resource "aws_instance" "test_ec2" {
+#   ami           = "ami-0ad522a4a529e7aa8" # Amazon Linux 2 AMI (Free tier eligible). Update based on your region.
+#   instance_type = "t2.micro"              # Free tier eligible
+#
+#   # Remove SSH-related key pair
+#   # key_name               = aws_key_pair.deployer_key.key_name
+#   security_groups        = [aws_security_group.allow_http.name]
+#   associate_public_ip_address = true
+#
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               sudo yum update -y
+#               sudo yum install -y httpd
+#               sudo systemctl start httpd
+#               sudo systemctl enable httpd
+#               echo "<h1>Deployed via Terraform</h1>" > /var/www/html/index.html
+#               EOF
+#
+#   tags = {
+#     Name = "GitOps-Test-Instance"
+#   }
+# }
+#
+# output "instance_public_ip" {
+#   value = aws_instance.test_ec2.public_ip
+# }
